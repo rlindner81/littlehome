70d314235b1a5ce73627cb3b39ea4bb34e06b4ad
Richard Lindner <richard.lindner@sap.com>
2020-08-31 12:52:56 +0200

    welcome page is not completely horrible

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,16 +1,67 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="row">
-      <div class="col">
-        <h1>Welcome</h1>
+  <q-page class="flex-block">
+    <div class="row q-pa-md q-col-gutter-md">
+      <div class="col-12">
+        <h4>
+          Welcome
+        </h4>
       </div>
-      <div class="col-6">
+
+      <div class="col-4">
         <q-img
-        srv=""></q-img>
+          src="welcome/rlindner.jpg"></q-img>
       </div>
-      <div class="col-6">Second column</div>
-      <div>Third column</div>
+      <div class="col-8">
+        <h5>
+        Dr. Richard Lindner
+        </h5>
+        <a
+          target="_blank" href="https://www.sap.com">
+          SAP SE
+        </a><br/>
+        Dietmar-Hopp-Allee 16<br/>
+        69190 Walldorf<br/>
+        Germany<br/>
+        <br/>
+        <a
+          href="mailto:rlindner@gmail.com">
+          rlindner81@gmail.com
+        </a>
+      </div>
+
+      <div class="col-4">
+        <router-link
+          to="/crypto">
+          <q-img
+            src="welcome/crypto.png"></q-img>
+        </router-link>
+      </div>
+      <div class="col-4">
+        <router-link
+          to="/math">
+          <q-img
+            src="welcome/math.png"></q-img>
+        </router-link>
+      </div>
+      <div class="col-4">
+        <router-link
+          to="/theater">
+          <q-img
+            src="welcome/theater.png"></q-img>
+        </router-link>
+      </div>
     </div>
+<!--    <div class="row">-->
+<!--      <div class="col">-->
+<!--        Welcome-->
+<!--      </div>-->
+<!--    </div>-->
+
+<!--    <div class="row">-->
+<!--      <div class="col-6">-->
+<!--      </div>-->
+<!--      <div class="col-6">Second column</div>-->
+<!--    </div>-->
   </q-page>
 </template>
 
