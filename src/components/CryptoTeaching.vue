5a8e16c1f75f3bac7cb24ebba4d8c9b5cd4d2b8c
Richard Lindner <rlindner81@gmail.com>
2020-11-28 12:38:47 +0100

    crypto-teaching

---
+++ src/components/CryptoTeaching.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <p>
+      <!-- name -->
+      <b>{{ name }}</b><br />
+      <span v-if="lectures && lectures.length">
+        <span
+          v-for="(lecture,index) in lectures"
+          v-bind="lecture"
+          :key="index"
+        >
+          {{ lecture.name }}
+          [
+          <span
+            v-for="(variant, index) in lecture.variants"
+            v-bind="variant"
+            :key="index"
+          >
+            <a
+              :href="variant.link">{{ variant.name }}</a>
+            <span
+              v-if="index < lecture.variants.length - 1"
+            >, </span>
+          </span>
+        ]
+        <br />
+        </span>
+      </span>
+
+      <!-- teachers -->
+      <span
+        v-for="(person, personIndex) in teachers"
+        v-bind="person"
+        :key="person.name"
+      >
+        <span>{{ person.name }}</span>
+        <span
+          v-if="personIndex < teachers.length - 1"
+        >, </span>
+      </span><br />
+
+      <!-- semester -->
+      <span>{{ semester }}</span><br />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CryptoTeaching',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+
+    lectures: {
+      type: Array,
+      default: () => []
+    },
+
+    teachers: {
+      type: Array,
+      required: true
+    },
+
+    semester: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
