7efc1e581e18b930ee4ddcd14f46e845cd5654de
Richard Lindner <richard.lindner@sap.com>
2021-07-24 09:20:18 +0200

    use exact links to avoid sticky router active class
    https://github.com/quasarframework/quasar/issues/6885

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -3,6 +3,7 @@
     clickable
     tag="a"
     :to="link"
+    exact
   >
     <q-item-section
       v-if="icon"
@@ -38,7 +39,7 @@ export default defineComponent({
 
     link: {
       type: String,
-      default: '#'
+      default: ''
     },
 
     icon: {
