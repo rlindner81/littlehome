97cde288a02a31417c2a833c7e3869f714fb74c1
Richard Lindner <rlindner81@gmail.com>
2022-01-04 23:20:38 +0100

    even more polish

--- src/pages/Recipes.vue
+++ src/pages/Recipes.vue
@@ -12,29 +12,29 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="col-4">
-        <q-card-section class="bg-secondary text-white">
-          2 große Zwiebeln,<br />
-          1500g Hackfleisch,<br />
-          2 große Zehen Knoblauch,<br />
-          800g gehackte Tomaten,<br />
-          1000g passierte Tomaten,<br />
-          4 Lorbeerblätter,<br />
-          Olivenöl<br />
-          Zucker, Salz und Pfeffer<br />
-          <br />
-          200g Spaghetti/Person<br />
-          <br />
-          Zeitaufwand: ca. 1,5 Stunden
-        </q-card-section>
-      </q-card>
+      <q-card class="col-12">
+        <q-card-section horizontal>
+          <q-card-section class="col-4 bg-secondary text-white">
+            2 große Zwiebeln,<br />
+            1500g Hackfleisch,<br />
+            2 große Zehen Knoblauch,<br />
+            800g gehackte Tomaten,<br />
+            1000g passierte Tomaten,<br />
+            4 Lorbeerblätter,<br />
+            Olivenöl<br />
+            Zucker, Salz und Pfeffer<br />
+            <br />
+            200g Spaghetti/Person<br />
+            <br />
+            Zeitaufwand: ca. 1,5 Stunden
+          </q-card-section>
 
-      <q-card class="col-8">
-        <q-card-section>
-          Die Zwiebeln in kleine und den Knoblauch in sehr kleine Würfel schneiden.
-          Den Boden eines Topfs mit Öl bedecken und die Zwiebeln im Öl glasig braten. Am Ende das Hackfleisch dazugeben und unter ständigem umrühren ebenfalls anbraten. Dabei mit reichlich Salz, Pfeffer und Kräutern der Provence würzen und mit dem Löffel das Fleisch so zerkleinern, dass keine Brocken entstehen und alles gleichmässig durchbrät.
-          Wenn das Fleisch größtenteils durch ist, kommen die gehackten und passierten Tomaten dazu und man rührt alles um bis ein Brei entsteht. Jetzt kommen die Lorbeerblätter, ein halber Teelöffel Zucker und der gewürfelte Knoblauch hinzu. Unter gelegentlichem Rühren mindestens 0,5 Stunden kochen lassen bis der Brei dickflüssig ist.
-          Parallel die Nudeln kochen.
+          <q-card-section>
+            Die Zwiebeln in kleine und den Knoblauch in sehr kleine Würfel schneiden.
+            Den Boden eines Topfs mit Öl bedecken und die Zwiebeln im Öl glasig braten. Am Ende das Hackfleisch dazugeben und unter ständigem umrühren ebenfalls anbraten. Dabei mit reichlich Salz, Pfeffer und Kräutern der Provence würzen und mit dem Löffel das Fleisch so zerkleinern, dass keine Brocken entstehen und alles gleichmässig durchbrät.
+            Wenn das Fleisch größtenteils durch ist, kommen die gehackten und passierten Tomaten dazu und man rührt alles um bis ein Brei entsteht. Jetzt kommen die Lorbeerblätter, ein halber Teelöffel Zucker und der gewürfelte Knoblauch hinzu. Unter gelegentlichem Rühren mindestens 0,5 Stunden kochen lassen bis der Brei dickflüssig ist.
+            Parallel die Nudeln kochen.
+          </q-card-section>
         </q-card-section>
       </q-card>
 
@@ -44,22 +44,22 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="col-4">
-        <q-card-section class="bg-secondary text-white">
-          6 Tomaten,<br />
-          250g Mozzarella,<br />
-          1EL Olivenöl,<br />
-          1TL Weißweinessig,<br />
-          Knoblauch, Basilikum,<br />
-          Pfeffer, Salz<br />
-          <br />
-          Für 4 Personen
-        </q-card-section>
-      </q-card>
+      <q-card class="col-12">
+        <q-card-section horizontal>
+          <q-card-section class="col-4 bg-secondary text-white">
+            6 Tomaten,<br />
+            250g Mozzarella,<br />
+            1EL Olivenöl,<br />
+            1TL Weißweinessig,<br />
+            Knoblauch, Basilikum,<br />
+            Pfeffer, Salz<br />
+            <br />
+            Für 4 Personen
+          </q-card-section>
 
-      <q-card class="col-8">
-        <q-card-section>
-          Aus Olivenöl, Weißweinessig, frisch gemahlenem Pfeffer und Salz eine würzige Marinade bereiten. Knoblauch pellen, durch die Knoblauchpresse drücken und in die Marinade rühren. Die Hälfte vom frischen Basilikum in feine Streifen schneiden und in die Marinade geben. Die Tomaten und den Mozzarella mit einem scharfen Tomatenmesser in Scheiben schneiden und ca. 15 Minuten in der Marinade ziehen lassen. Tomaten mit Mozzarella auf einem großen Teller im Wechsel leicht überlappend anrichten. Vom restlichen frischen Basilikum die Blätter abzupfen und kurz vor dem Servieren über die Tomaten mit Mozzarella streuen.
+          <q-card-section>
+            Aus Olivenöl, Weißweinessig, frisch gemahlenem Pfeffer und Salz eine würzige Marinade bereiten. Knoblauch pellen, durch die Knoblauchpresse drücken und in die Marinade rühren. Die Hälfte vom frischen Basilikum in feine Streifen schneiden und in die Marinade geben. Die Tomaten und den Mozzarella mit einem scharfen Tomatenmesser in Scheiben schneiden und ca. 15 Minuten in der Marinade ziehen lassen. Tomaten mit Mozzarella auf einem großen Teller im Wechsel leicht überlappend anrichten. Vom restlichen frischen Basilikum die Blätter abzupfen und kurz vor dem Servieren über die Tomaten mit Mozzarella streuen.
+          </q-card-section>
         </q-card-section>
       </q-card>
 
@@ -70,22 +70,22 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="col-4">
-        <q-card-section class="bg-secondary text-white">
-          500ml Milch,<br />
-          250g Mehl,<br />
-          3 Eier,<br />
-          1 Esslöffel Öl<br />
-          <br />
-          Optional:<br />
-          Eier, Schinken, Reibekäse (herzhaft) oder<br />
-          Zimt und Zucker, Nutella, Apfelmus (süß)
-        </q-card-section>
-      </q-card>
+      <q-card class="col-12">
+        <q-card-section horizontal>
+          <q-card-section class="col-4 bg-secondary text-white">
+            500ml Milch,<br />
+            250g Mehl,<br />
+            3 Eier,<br />
+            1 Esslöffel Öl<br />
+            <br />
+            Optional:<br />
+            Eier, Schinken, Reibekäse (herzhaft) oder<br />
+            Zimt und Zucker, Nutella, Apfelmus (süß)
+          </q-card-section>
 
-      <q-card class="col-8">
-        <q-card-section>
-          Eine große Schüssel mit dem Mehl und den Eiern füllen. Dann das Öl reingeben und langsam die Milch unterrühren. Die fertige Mixtur ne Stunde (oder bis zur Verwendung) in den Kühlschrank stellen. Beim Zubereiten den Pfannenboden mit Öl bestreichen und möglichst dünne, runde Crêpes machen und herzhaft oder süß belegen.
+          <q-card-section>
+            Eine große Schüssel mit dem Mehl und den Eiern füllen. Dann das Öl reingeben und langsam die Milch unterrühren. Die fertige Mixtur ne Stunde (oder bis zur Verwendung) in den Kühlschrank stellen. Beim Zubereiten den Pfannenboden mit Öl bestreichen und möglichst dünne, runde Crêpes machen und herzhaft oder süß belegen.
+          </q-card-section>
         </q-card-section>
       </q-card>
 
@@ -96,36 +96,36 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="col-4">
-        <q-card-section class="bg-secondary text-white">
-          500g Mehl,<br />
-          4 Eier,<br />
-          200g Zucker,<br />
-          250g Butter,<br />
-          1/4l Milch,<br />
-          1pk Vanillezucker + 1pk Backpulver,<br />
-          <br />
-          Optional:<br />
-          Puderzucker, Zimt und Zucker, Nutella, Apfelmus (süß)
-        </q-card-section>
-      </q-card>
+      <q-card class="col-12">
+        <q-card-section horizontal>
+          <q-card-section class="col-4 bg-secondary text-white">
+            500g Mehl,<br />
+            4 Eier,<br />
+            200g Zucker,<br />
+            250g Butter,<br />
+            1/4l Milch,<br />
+            1pk Vanillezucker + 1pk Backpulver,<br />
+            <br />
+            Optional:<br />
+            Puderzucker, Zimt und Zucker, Nutella, Apfelmus (süß)
+          </q-card-section>
 
-      <q-card class="col-8">
-        <q-card-section>
-          <ul>
-            <li>
-              Butter in der Microwelle flüssig machen.
-            </li>
-            <li>
-              Mehl, Backpulver, Zucken, Vanillezucker + flüssige Butter in eine große Schüssel geben und mit einem Mixer gut vermischen.
-            </li>
-            <li>
-              Die Eier aufschlagen und hinzugeben. Danach wieder verrühren und dabei die Milch reingießen bis eine erreicht ist, bei der der Teig zäh vom Löffel tropft und keine Klumpen vorhanden sind.
-            </li>
-            <li>
-              Damit ist der Teig fertig. Das Waffeleisen auf 5/6 stärke stellen und ca. 2el Teig / Waffel benutzen.
-            </li>
-          </ul>
+          <q-card-section>
+            <ul>
+              <li>
+                Butter in der Microwelle flüssig machen.
+              </li>
+              <li>
+                Mehl, Backpulver, Zucken, Vanillezucker + flüssige Butter in eine große Schüssel geben und mit einem Mixer gut vermischen.
+              </li>
+              <li>
+                Die Eier aufschlagen und hinzugeben. Danach wieder verrühren und dabei die Milch reingießen bis eine erreicht ist, bei der der Teig zäh vom Löffel tropft und keine Klumpen vorhanden sind.
+              </li>
+              <li>
+                Damit ist der Teig fertig. Das Waffeleisen auf 5/6 stärke stellen und ca. 2el Teig / Waffel benutzen.
+              </li>
+            </ul>
+          </q-card-section>
         </q-card-section>
       </q-card>
 
@@ -136,68 +136,68 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="col-4">
-        <q-card-section class="bg-secondary text-white">
-          2 (teifgekühlte) Entenbrüste<br />
-          0,2l Rotwein<br />
-          1 Orange<br />
-          2tlf hartgefrohrene Butter<br />
-          4 Zwiebeln<br />
-          Salz, Pfeffer<br />
-          <br />
-          Optional:<br />
-          Baguette, Rotwein (zum trinken)<br />
-          <br />
-          Hilfreich:<br />
-          Pinsette zum Entfernen der Federkiele<br />
-          Küchenrolle zum Abtrocknen nach dem Auftaun<br />
-          Scharfes Messer zum gezielten Einschneiden
-        </q-card-section>
-      </q-card>
+      <q-card class="col-12">
+        <q-card-section horizontal>
+          <q-card-section class="col-4 bg-secondary text-white">
+            2 (teifgekühlte) Entenbrüste<br />
+            0,2l Rotwein<br />
+            1 Orange<br />
+            2tlf hartgefrohrene Butter<br />
+            4 Zwiebeln<br />
+            Salz, Pfeffer<br />
+            <br />
+            Optional:<br />
+            Baguette, Rotwein (zum trinken)<br />
+            <br />
+            Hilfreich:<br />
+            Pinsette zum Entfernen der Federkiele<br />
+            Küchenrolle zum Abtrocknen nach dem Auftaun<br />
+            Scharfes Messer zum gezielten Einschneiden
+          </q-card-section>
 
-      <q-card class="col-8">
-        <q-card-section>
-          <ul>
-            <li>
-              Die Entenbrust eine Nacht lang im Kühlschrank lagern zum Abtauen und die Butter ins Teifkühlfach tun zum Einfrieren.
-            </li>
-            <li>
-              Zwiebeln möglichst klein Würfeln.
-            </li>
-            <li>
-              Entenbrüste abtrocknen (mit Küchenrolle) und Federkiele entfernen.
-            </li>
-            <li>
-              Haut in Rautenmuster einschneiden, dabei nicht das Fleisch verletzen.
-            </li>
-            <li>
-              Pfanne aufheizen. Bei maximaler Hitze[10] beide Entenbrüste auf Hautseite scharf anbraten. Sobald die Ente in der Pfanne ist die Hitze reduzieren[6].
-            </li>
-            <li>
-              Backofen auf 80 Grad vorheizen.
-            </li>
-            <li>
-              Nach 5min oder wenn die Hautseite knusprig braun ist die Fleischseite Pfeffern und beide Brüste auf die Fleischseite drehen.
-            </li>
-            <li>
-              Nach weiteren 5min die Entenbrüste aus der Pfanne nehmen und im Backofen warm halten.
-            </li>
-            <li>
-              Das ausgelassene Fett salzen und die gewürfelten Zwiebeln darin glasig braten. Währendessen die Orange filetieren und dabei den Saft abtrennen.
-            </li>
-            <li>
-              Entenfett und Zwiebeln mit Orangensaft und Rotwein aufgießen und danach aufkochen bis die Flüssigkeit halbiert ist. Abschmecken, nachsalzen/pfeffern.
-            </li>
-            <li>
-              Beim Aufkochen der Soße die Ente aus dem Backofen nehmen und jetzt auch das Fleisch einschneiden aber nur quer und nicht längs. Den austretenden Saft zur Soße geben.
-            </li>
-            <li>
-              Die Soße mit der Butter aufmontieren. Dann Orangenfilets und Ente hinzugeben und bei geringster Temperatur[1] ca. 5min zeihen lassen. Die Ente dabei mit Soßenflüssigkeit übergiessen.
-            </li>
-            <li>
-              Mit Baguette und Rotwein servieren.
-            </li>
-          </ul>
+          <q-card-section>
+            <ul>
+              <li>
+                Die Entenbrust eine Nacht lang im Kühlschrank lagern zum Abtauen und die Butter ins Teifkühlfach tun zum Einfrieren.
+              </li>
+              <li>
+                Zwiebeln möglichst klein Würfeln.
+              </li>
+              <li>
+                Entenbrüste abtrocknen (mit Küchenrolle) und Federkiele entfernen.
+              </li>
+              <li>
+                Haut in Rautenmuster einschneiden, dabei nicht das Fleisch verletzen.
+              </li>
+              <li>
+                Pfanne aufheizen. Bei maximaler Hitze[10] beide Entenbrüste auf Hautseite scharf anbraten. Sobald die Ente in der Pfanne ist die Hitze reduzieren[6].
+              </li>
+              <li>
+                Backofen auf 80 Grad vorheizen.
+              </li>
+              <li>
+                Nach 5min oder wenn die Hautseite knusprig braun ist die Fleischseite Pfeffern und beide Brüste auf die Fleischseite drehen.
+              </li>
+              <li>
+                Nach weiteren 5min die Entenbrüste aus der Pfanne nehmen und im Backofen warm halten.
+              </li>
+              <li>
+                Das ausgelassene Fett salzen und die gewürfelten Zwiebeln darin glasig braten. Währendessen die Orange filetieren und dabei den Saft abtrennen.
+              </li>
+              <li>
+                Entenfett und Zwiebeln mit Orangensaft und Rotwein aufgießen und danach aufkochen bis die Flüssigkeit halbiert ist. Abschmecken, nachsalzen/pfeffern.
+              </li>
+              <li>
+                Beim Aufkochen der Soße die Ente aus dem Backofen nehmen und jetzt auch das Fleisch einschneiden aber nur quer und nicht längs. Den austretenden Saft zur Soße geben.
+              </li>
+              <li>
+                Die Soße mit der Butter aufmontieren. Dann Orangenfilets und Ente hinzugeben und bei geringster Temperatur[1] ca. 5min zeihen lassen. Die Ente dabei mit Soßenflüssigkeit übergiessen.
+              </li>
+              <li>
+                Mit Baguette und Rotwein servieren.
+              </li>
+            </ul>
+          </q-card-section>
         </q-card-section>
       </q-card>
 
@@ -208,47 +208,47 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="col-4">
-        <q-card-section class="bg-secondary text-white">
-          1 (teifgekühlte) Gans<br />
-          1 Suppengrün
-        </q-card-section>
-      </q-card>
+      <q-card class="col-12">
+        <q-card-section horizontal>
+          <q-card-section class="col-4 bg-secondary text-white">
+            1 (teifgekühlte) Gans<br />
+            1 Suppengrün
+          </q-card-section>
 
-      <q-card class="col-8">
-        <q-card-section>
-          <ul>
-            <li>
-              Aus dem Tiefkühlfach und der Plastikfolie nehmen und einen Abend lang bei Zimmertemperatur mit Tuchabdeckung auftauen lassen.
-            </li>
-            <li>
-              Innereien rausnehmen und innen und aussen waschen.
-            </li>
-            <li>
-              Die Gans zusammen mit dem Suppengrün in den Bräter legen und mit Wasser füllen bis der Wasserstand in dem Bräter ca. 3cm (2 Fingerbreit) beträgt.
-            </li>
-            <li>
-              Mit Brustseite nach _unten_ im vorgeheizten Ofen bei 220° anbraten.
-            </li>
-            <li>
-              Nach 20-30min Hitze auf 180-190° reduzieren. Pro Kilo der Gans eine Stunde Backzeit rechnen.
-            </li>
-            <li>
-              Die Gans immer wieder übergiessen mit Brühe und Brühe nachgiessen, um den Wasserstand zu halten.
-            </li>
-            <li>
-              Gelegentlich seitlich Haut einstechen damit das Fett abfliessen kann.
-            </li>
-            <li>
-              Wenn die Oberseite gebräunt ist wird die Gans gewendet (Brust nach _oben_).
-            </li>
-            <li>
-              Wenn die Unterseite auch gebräunt ist die Gans auf dem Rost für 10-20min 'grillen', damit Haut schön knusprig wird. Zusätzlich mit kaltem Wasser beträufeln.
-            </li>
-            <li>
-              Fett von Bratensauce abgiessen / abschöpfen.
-            </li>
-          </ul>
+          <q-card-section>
+            <ul>
+              <li>
+                Aus dem Tiefkühlfach und der Plastikfolie nehmen und einen Abend lang bei Zimmertemperatur mit Tuchabdeckung auftauen lassen.
+              </li>
+              <li>
+                Innereien rausnehmen und innen und aussen waschen.
+              </li>
+              <li>
+                Die Gans zusammen mit dem Suppengrün in den Bräter legen und mit Wasser füllen bis der Wasserstand in dem Bräter ca. 3cm (2 Fingerbreit) beträgt.
+              </li>
+              <li>
+                Mit Brustseite nach _unten_ im vorgeheizten Ofen bei 220° anbraten.
+              </li>
+              <li>
+                Nach 20-30min Hitze auf 180-190° reduzieren. Pro Kilo der Gans eine Stunde Backzeit rechnen.
+              </li>
+              <li>
+                Die Gans immer wieder übergiessen mit Brühe und Brühe nachgiessen, um den Wasserstand zu halten.
+              </li>
+              <li>
+                Gelegentlich seitlich Haut einstechen damit das Fett abfliessen kann.
+              </li>
+              <li>
+                Wenn die Oberseite gebräunt ist wird die Gans gewendet (Brust nach _oben_).
+              </li>
+              <li>
+                Wenn die Unterseite auch gebräunt ist die Gans auf dem Rost für 10-20min 'grillen', damit Haut schön knusprig wird. Zusätzlich mit kaltem Wasser beträufeln.
+              </li>
+              <li>
+                Fett von Bratensauce abgiessen / abschöpfen.
+              </li>
+            </ul>
+          </q-card-section>
         </q-card-section>
       </q-card>
 
