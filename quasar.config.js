54655e6fa3e6f01f38ccf375aec50e1fa23f2ede
Richard Lindner <rlindner81@gmail.com>
2024-10-09 10:24:49 +0200

    back to being hosted under top-level of subdomain

--- quasar.config.js
+++ quasar.config.js
@@ -58,7 +58,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/littlehome',
+      // publicPath: '/littlehome',
       // analyze: true,
       // env: {},
       // rawDefine: {}
